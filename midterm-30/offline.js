2238f1c6f4ed469598c482c17e25863cf0e00cf1
nanakanthira <s42888@bj.ac.th>
2024-12-27 15:37:45 +0700

    Add files via upload

---
+++ midterm-30/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1735288668,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/player-sheet0.png",
+		"images/salmon-sheet0.png",
+		"images/treasurebox-sheet0.png",
+		"images/ground.png",
+		"images/sprite-sheet0.png",
+		"images/skyblock2-sheet0.png",
+		"images/skyblock3-sheet0.png",
+		"images/skyblock4-sheet0.png",
+		"images/enemy-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
